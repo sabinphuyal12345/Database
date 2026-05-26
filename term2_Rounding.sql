0bd087d7a88db01f2eb7bcfbf1eb2c1d2ce0760d
Sabin Phuyal <sabin.20251081@nami.edu.np>
2026-05-26 20:57:10 +0545

    Add files via upload

--- term2_Rounding.sql
+++ term2_Rounding.sql
@@ -10,6 +10,17 @@ VALUES('sabin',20000);
 SELECT CEIL(salary)
 FROM stud;
 
+--more examples
+
+SELECT CEIL(notes)
+FROM bank;
+
+SELECT CEIL(currency)
+FROM exchange;
+
+SELECT CEIL(student)
+FROM schools;
+
 --substr
 CREATE TABLE stud(
     name VARCHAR2(10),
@@ -21,6 +32,17 @@ VALUES('sabin',20000);
 
 SELECT SUBSTR('sabin',2,3)
 FROM stud;
+
+--more examples
+
+SELECT SUBSTR('sabin',2,3)
+FROM stud;
+
+SELECT SUBSTR('rabin',2,3)
+FROM staffs;
+
+SELECT SUBSTR('nabin',2,3)
+FROM employees;
 
 --Trim
 
@@ -35,6 +57,17 @@ VALUES('sabin',20000);
 SELECT TRIM('a')
 FROM stud;
 
+--more examples
+
+SELECT TRIM('n')
+FROM stud;
+
+SELECT TRIM('f')
+FROM employees;
+
+SELECT TRIM('g')
+FROM staffs;
+
 --Length
 CREATE TABLE stud(
     name VARCHAR2(10),
@@ -46,6 +79,17 @@ VALUES('sabin',20000);
 
 SELECT LENGTH('sabin')
 FROM stud;
+
+--more examples
+
+SELECT LENGTH('nepal')
+FROM countries;
+
+SELECT LENGTH('addition')
+FROM maths;
+
+SELECT LENGTH('sabinphuyalmulpani')
+FROM persons;
 
 --instr 
 CREATE TABLE stud(
@@ -59,6 +103,17 @@ VALUES('sabin',20000);
 SELECT INSTR('sabin','in',3,2)
 FROM stud;
 
+--more examples
+
+SELECT INSTR('rabin','in',3,2)
+FROM employees;
+
+SELECT INSTR('nabin','in',3,2)
+FROM staffs;
+
+SELECT INSTR('neymar','ar',4,2)
+FROM players;
+
 --concat
 CREATE TABLE stud(
     name VARCHAR2(10),
@@ -70,6 +125,17 @@ VALUES('sabin',20000);
 
 SELECT CONCAT('sabin','sp')
 FROM stud;
+
+--more examples
+
+SELECT CONCAT('sabin','phuyal')
+FROM employees;
+
+SELECT CONCAT('You','Tube')
+FROM medias;
+
+SELECT CONCAT('Nepal','Asia')
+FROM continents;
 
 ---End----
 
